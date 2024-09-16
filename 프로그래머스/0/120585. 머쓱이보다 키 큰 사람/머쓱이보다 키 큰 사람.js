d8a66d2a8c074637bfe6517dbc492f0bca72e084
Gayeong Kim <155832049+gouoy@users.noreply.github.com>
2024-09-16 12:57:38 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,5 +1,6 @@
 function solution(array, height) {
     var answer = 0;
-    array.map((i)=> i>height? answer+=1 : null)
+    // array.map((i)=> i>height? answer+=1 : null)
+    array.forEach((i)=> i>height? answer+=1:null)
     return answer;
 }
