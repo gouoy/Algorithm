c28aa36bfbd7d688de45558a8a18f2359fdfcab8
Gayeong Kim <155832049+gouoy@users.noreply.github.com>
2024-09-26 17:26:06 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,5 +1,4 @@
 function solution(num_list, n) {
-    var answer = [[1,2]];
     const numLeng = num_list.length 
     let arr = Array.from(Array(numLeng/n), ()=>Array(n).fill(0)) 
     for(let i=0;i<numLeng/n;i++){
