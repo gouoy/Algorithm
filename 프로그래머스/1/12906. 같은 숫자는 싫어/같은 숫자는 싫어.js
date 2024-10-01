a1881c5d3c4786153da3426b4556f3a7ecdce982
Gayeong Kim <155832049+gouoy@users.noreply.github.com>
2024-10-01 14:26:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.49 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -7,10 +7,7 @@ function solution(arr)
     for(let i=0; i<arr.length; i++){
         if(answer[answer.length-1] != arr[i]){
             answer.push(arr[i])
-        }else{
-            continue; 
         }
-        
     }
     return answer;
 }
