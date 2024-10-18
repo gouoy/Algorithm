4f16494afc1f40ccd0f7f2e422b2e484199b84ce
Gayeong Kim <155832049+gouoy@users.noreply.github.com>
2024-10-18 11:25:51 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 1.09 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    var answer = [];
+    for(let i=0;i<arr.length;i++){ 0
+        for(let j=0; j<arr[i];j++){ 
+            answer.push(arr[i])
+        }
+    }
+    return answer;
+}
