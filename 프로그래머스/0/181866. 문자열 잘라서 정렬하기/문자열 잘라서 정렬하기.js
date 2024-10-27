2643cad32fd5bfd3e73bf2714af2b8319ee1ec9a
Gayeong Kim <155832049+gouoy@users.noreply.github.com>
2024-10-28 03:03:41 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 19.56 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -0,0 +1,9 @@
+function solution(myString) {
+    var list = myString.split('x').sort()
+    while(list[0] == ""){
+          let result = list.shift()
+          
+          
+    }
+    return list
+}
